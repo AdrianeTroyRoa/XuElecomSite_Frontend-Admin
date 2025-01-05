8d74ff3a3e6dbbddcb82e826f2d218e7303acfb6
Troy Roa <roatroy850@gmail.com>
2025-01-05 14:54:10 +0800

    feat: added other misc files for tailwind and login

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,vue,ts}",
+    "./app.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
