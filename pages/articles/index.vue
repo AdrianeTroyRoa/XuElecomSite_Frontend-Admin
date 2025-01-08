58e3bf4f524d1cc22aaa43279bae49cd50840174
Troy Roa <roatroy850@gmail.com>
2025-01-08 22:44:09 +0800

    feat: copied index page to /articles

---
+++ pages/articles/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar />
+  <div v-for="(item, key) in someSample">
+    <ArticleItem
+      :title="item.title"
+      :status="item.status"
+      :description="item.description"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const theTitle = "Some dick";
+const someSample = [
+  { title: "Hello", status: false, description: "blind-sided" },
+  { title: "Goodbye", status: true, description: "hand-sided" },
+  { title: "Hi again", status: false, description: "head-sided" },
+];
+</script>
