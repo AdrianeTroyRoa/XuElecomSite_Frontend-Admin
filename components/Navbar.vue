4a3ea035b6fffc4a7e8a232338513a5696bce7ef
Troy Roa <roatroy850@gmail.com>
2025-01-11 23:09:02 +0800

    feat: added sidebar and  page title in the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,17 @@
 <template>
   <!--for margin purposes-->
-  <div class="bg-white w-full my-24"></div>
+  <div class="w-full my-24"></div>
 
   <!--fixed-positioned navbar-->
-  <div
-    class="navbar flex bg-gray-100 fixed w-full top-0 z-20 p-4 justify-between"
-  >
-    <!-- Logo -->
-    <div class="">
-      <a class="btn btn-ghost hover:bg-gray-100">
-        <div>
-          <div class="text-xl text-primary font-bold">BOTAR ATENISTA</div>
-          <div
-            class="rounded-full bg-[#FAD60B] p-1 px-12 shadow-black shadow-md font-bold"
-          >
-            THINK. ACT. VOTE.
-          </div>
-        </div>
-      </a>
+  <div class="navbar flex justify-center items-center bg-gray-100 fixed w-full top-0 z-20 p-4">
+    <!-- Sidebar button -->
+    <div class="flex-1">
+      <Sidebar />
     </div>
+    <div class="flex-1 text-2xl font-bold">Articles</div>
 
     <!-- Logout Button -->
-    <div>
+    <div class="flex-none">
       <button class="btn btn-primary btn-ghost">Logout</button>
     </div>
   </div>
---
+++ components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div className="drawer">
+    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+    <div className="drawer-content">
+      <label htmlFor="my-drawer" className="btn drawer-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <rect y="4" width="24" height="2"></rect>
+          <rect y="11" width="24" height="2"></rect>
+          <rect y="18" width="24" height="2"></rect>
+        </svg>
+      </label>
+    </div>
+
+    <div className="drawer-side">
+      <label
+        htmlFor="my-drawer"
+        aria-label="close sidebar"
+        className="drawer-overlay"
+      ></label>
+      <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <button class="btn btn-ghost hover:bg-base-200 mb-12">
+          <div>
+            <div class="text-xl text-primary font-bold">BOTAR ATENISTA</div>
+            <div
+              class="rounded-full bg-[#FAD60B] p-1 px-12 shadow-black shadow-md font-bold"
+            >
+              THINK. ACT. VOTE.
+            </div>
+          </div>
+        </button>
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
