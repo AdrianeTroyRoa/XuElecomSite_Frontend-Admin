d3fb98e1a9c04abec45acce1b825e929937864dd
Troy Roa <roatroy850@gmail.com>
2025-01-14 11:49:42 +0800

    fix: sidebar now functioning properly

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,44 +1,71 @@
 <template>
-  <!--Something wrong here-->
-  <div class="drawer">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <label htmlFor="my-drawer" class="btn drawer-button">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <rect y="4" width="24" height="2"></rect>
-          <rect y="11" width="24" height="2"></rect>
-          <rect y="18" width="24" height="2"></rect>
-        </svg>
-      </label>
-    </div>
+  <div class="relative">
+    <!-- Hamburger Button -->
+    <button
+      @click="toggleSidebar"
+      class="fixed top-4 left-4 z-50 p-2 rounded-md"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        v-if="isSidebarOpen"
+        fill="white"
+      >
+        <rect y="4" width="24" height="2"></rect>
+        <rect y="11" width="24" height="2"></rect>
+        <rect y="18" width="24" height="2"></rect>
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        v-else
+      >
+        <rect y="4" width="24" height="2"></rect>
+        <rect y="11" width="24" height="2"></rect>
+        <rect y="18" width="24" height="2"></rect>
+      </svg>
+    </button>
 
-    <div class="drawer-side">
-      <label
-        htmlFor="my-drawer"
-        aria-label="close sidebar"
-        class="drawer-overlay"
-      ></label>
-      <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-        <button class="btn btn-ghost hover:bg-base-200 mb-12">
-          <div>
-            <div class="text-xl text-primary font-bold">BOTAR ATENISTA</div>
-            <div
-              class="rounded-full bg-[#FAD60B] p-1 px-12 shadow-black shadow-md font-bold"
-            >
-              THINK. ACT. VOTE.
-            </div>
-          </div>
-        </button>
-        <li><a class="text-xl font-bold" href="/articles">Articles</a></li>
-        <li><a class="text-xl font-bold" href="/questions">Questions</a></li>
+    <!-- Sidebar -->
+    <div
+      :class="{
+        'translate-x-0': isSidebarOpen,
+        '-translate-x-full': !isSidebarOpen,
+      }"
+      class="text-white fixed top-0 left-0 h-full w-64 bg-neutral transform transition-transform duration-300 ease-in-out"
+    >
+      <ul class="p-4 space-y-4 mt-24">
+        <li>
+          <a href="/" class="block p-2 rounded-md hover:bg-gray-700">Home</a>
+        </li>
+        <li>
+          <a href="/articles" class="block p-2 rounded-md hover:bg-gray-700">Articles</a>
+        </li>
+        <li>
+          <a href="/questions" class="block p-2 rounded-md hover:bg-gray-700">Questions</a>
+        </li>
       </ul>
     </div>
+
+    <!-- Overlay -->
+    <div
+      v-if="isSidebarOpen"
+      @click="toggleSidebar"
+      class="fixed inset-0 bg-black bg-opacity-50 lg:hidden z-30"
+    ></div>
   </div>
 </template>
 
-<script></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isSidebarOpen = ref(false);
+
+const toggleSidebar = () => {
+  isSidebarOpen.value = !isSidebarOpen.value;
+};
+</script>
