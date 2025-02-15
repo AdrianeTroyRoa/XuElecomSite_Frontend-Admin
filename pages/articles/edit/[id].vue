bd551ca7cc1be1277290a35ee4f80226e629c043
Troy Roa <roatroy850@gmail.com>
2025-02-15 12:19:10 +0800

    feat: enabled shift from live to draft and vice versa when saving articles

--- pages/articles/edit/[id].vue
+++ pages/articles/edit/[id].vue
@@ -3,7 +3,11 @@
     <div class="flex-1">
       <Sidebar title="Article Editor" />
     </div>
-    <button class="btn btn-secondary flex-none" @click="showInputtedContent">
+    <button
+      class="btn btn-secondary flex-none"
+      @click="showInputtedContent"
+      onclick="document.getElementById('popup-modal').showModal()"
+    >
       Save
     </button>
   </div>
@@ -37,6 +41,71 @@
       </div>
     </div>
   </div>
+
+  <dialog class="modal" id="popup-modal">
+    <div class="modal-box">
+      <form method="dialog">
+        <button
+          type="button"
+          class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+          data-modal-hide="popup-modal"
+        >
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span class="sr-only">Close modal</span>
+        </button>
+        <div class="p-4 md:p-5 text-center">
+          <svg
+            class="mx-auto mb-4 text-gray-400 w-12 h-12"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h3 class="mb-5 text-lg font-normal text-gray-500">
+            Take this article live?
+          </h3>
+          <button
+            data-modal-hide="popup-modal"
+            type="button"
+            class="text-white bg-green-600 hover:bg-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+            @click="updateStatus(true)"
+          >
+            Yes
+          </button>
+          <button
+            data-modal-hide="popup-modal"
+            type="button"
+            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900e bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+            @click="updateStatus(false)"
+          >
+            No
+          </button>
+        </div>
+      </form>
+    </div>
+  </dialog>
 </template>
 
 <script setup lang="ts">
@@ -105,5 +174,24 @@ const renderMarkdown = computed(() =>
 
 function showInputtedContent() {
   console.log(markdownContent.value);
+}
+
+//saving edits
+const router = useRouter();
+const status = ref(false);
+
+function updateStatus(val) {
+  status.value = val;
+  submitEdits();
+}
+
+async function submitEdits() {
+  const { data, error } = await supabase
+    .from("Posts")
+    .update({ content: markdownContent.value, status: status.value })
+    .eq("id", route.params.id);
+  if (error) console.error("❌ failed to edit content");
+  else console.info("✅ successfully edited");
+  router.push("/articles");
 }
 </script>
