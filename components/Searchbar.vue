320df9426347bb60ce9cda0691c03881e953b29b
Troy Roa <roatroy850@gmail.com>
2025-07-09 20:33:22 +0800

    feat-update: change placeholder for searchbar

--- components/Searchbar.vue
+++ components/Searchbar.vue
@@ -5,7 +5,7 @@
         <input
           v-model="localSearchQuery"
           class="w-full bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-300 rounded-lg pl-4 pr-28 py-2.5 transition duration-200 ease-in-out focus:outline-none focus:border-slate-500 hover:border-slate-400 shadow-sm focus:shadow"
-          placeholder="Search articles..."
+          placeholder="Search through..."
         />
         <button
           class="absolute top-1/2 right-2 -translate-y-1/2 flex items-center justify-center rounded-md bg-slate-800 py-2 px-4 text-sm text-white transition duration-200 hover:bg-slate-700 focus:bg-slate-700 active:bg-slate-600 shadow-sm hover:shadow-md focus:outline-none disabled:pointer-events-none disabled:opacity-50"
